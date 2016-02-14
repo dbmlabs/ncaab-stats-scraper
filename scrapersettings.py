3c65d2c855a61730c73d093c1a93deed83f0b0d4
numericOverflow <github_com@bytality.com>
2016-02-14 12:00:39 -0600

    Reworked so year_index is pulled from a dictionary
    The user can now update the code for new academic years by adding a line to define the academic_year/year_index to the dictionary

--- scrapersettings.py
+++ scrapersettings.py
@@ -57,5 +57,4 @@ http_header = {
             } # Variables from the HTTP header (default)
 
 start_url = 'http://stats.ncaa.org/team/inst_team_list?sport_code=MBB&academic_year=' + str(academic_year) + "&division=1" # URL to start from (Change this for different years). You can get this URL from http://stats.ncaa.org/team/inst_team_list?sport_code=MBB&division=1. This URL is for the 2011-2012 season.
-
 domain_base = 'http://stats.ncaa.org' # Base domain
